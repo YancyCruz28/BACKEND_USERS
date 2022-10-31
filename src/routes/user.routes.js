e10470a77e2a64661ddc38a805b73a2de9fee97b
YancyCruz28 <114117712+YancyCruz28@users.noreply.github.com>
2022-10-30 19:29:37 -0500

    cambios, funcion completa con filtro email

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -16,10 +16,23 @@ const User = require("../models/user.models")
 
 // crear producto
 router.post("/create-user", async (req, res) =>{
+    // variable para encotrar el email en la base de datos
+    let {email} = req.body
+    // let email = req.body.email
+    // se usa para que el email quede escrito en minuscula 
+    email = email.toLowerCase()
+    req.body.email = email
+
+    // Obtener los ususrios que tienen ese email
+    let userExists = await User.find({email: email})
+    console.log(userExists)
+
+    // Validar si el email existe
+    if (userExists.length !== 0){
+        return res.status(400).json({msg: "El ususario ya existe"})
+    }
     
     // funcion para que la consola imprima lo que se coloca en el body de insomnia
-    
-
     const newUser = new User(req.body)
     await newUser.save()
     console.log(newUser)
